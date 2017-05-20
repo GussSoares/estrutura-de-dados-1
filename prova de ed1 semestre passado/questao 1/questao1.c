f6a4400d3a6f455a3aad0b970b3cdf8d5e2cc150
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-05-20 16:51:18 -0300

    Update questao1.c

--- prova de ed1 semestre passado/questao 1/questao1.c
+++ prova de ed1 semestre passado/questao 1/questao1.c
@@ -9,6 +9,7 @@ void repeticao(Lista* li, struct cliente cl){
 		for(i+1; i<li->qtd; i++){
 			if(strcmp(cli.cpf, li->dados[i+1].cpf) == 0){
 				remover_cliente(li, i+1);
+				repeticao(li, cl);
 			}
 		}
 	}else{
